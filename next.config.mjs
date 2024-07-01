29020526680aa0a3508d7d42404596aeec5983de
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-07-01 08:29:40 +0100

    update search component

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "53.fs1.hubspotusercontent-na1.net",
       },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
     ],
   },
 };
