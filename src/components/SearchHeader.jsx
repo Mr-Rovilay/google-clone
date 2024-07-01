700d48597d3c9ecf11fef8861127b286461e293e
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-07-01 08:42:37 +0100

    search header

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import SearchBox from "./SearchBox";
 import { RiSettings3Line } from "react-icons/ri";
 import { TbGridDots } from "react-icons/tb";
-// import SearchHeaderOptions from "./SearchHeaderOptions";
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 const SearchHeader = () => {
   return (
@@ -30,7 +30,7 @@ const SearchHeader = () => {
           Sign in
         </button>
       </div>
-      {/* <SearchHeaderOptions /> */}
+      <SearchHeaderOptions />
     </header>
   );
 };
